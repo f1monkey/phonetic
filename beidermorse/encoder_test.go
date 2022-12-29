5f8022b54d004c8a7321df5068d1444f2cc82d1c
Dmitry Solovov <cyradin@protonmail.com>
2022-12-29 01:38:32 +0100

    Add external API for bmpm encoder

---
+++ beidermorse/encoder_test.go
@@ -0,0 +1,33 @@
+package beidermorse
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/require"
+)
+
+func Test_Encoder_Encode(t *testing.T) {
+	cases := []struct {
+		name     string
+		mode     Mode
+		ruleset  Ruleset
+		input    string
+		expected []string
+	}{
+		{
+			name:     "ru_generic_exact",
+			mode:     Generic,
+			ruleset:  Exact,
+			input:    "апельсин",
+			expected: []string{"apelsin"},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.name, func(t *testing.T) {
+			e, err := NewEncoder(WithNameMode(c.mode), WithRuleset(c.ruleset))
+			require.NoError(t, err)
+			require.Equal(t, c.expected, e.Encode(c.input))
+		})
+	}
+}
