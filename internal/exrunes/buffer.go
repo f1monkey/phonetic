7aceb55cefa374f6f209bcf6d2635b1e17747a95
Dmitry Solovov <cyradin@protonmail.com>
2023-02-17 21:25:10 +0300

    Add rune buffer

---
+++ internal/exrunes/buffer.go
@@ -0,0 +1,113 @@
+package exrunes
+
+import "sync"
+
+type Buffer struct {
+	mtx   sync.Mutex
+	buf   []rune
+	index int
+	items map[int]bufferItem
+}
+
+func NewBuffer(length int) *Buffer {
+	return &Buffer{
+		buf:   make([]rune, 0, length),
+		items: make(map[int]bufferItem, length/10),
+	}
+}
+
+func (b *Buffer) Add(data []rune) int {
+	b.mtx.Lock()
+	defer b.mtx.Unlock()
+
+	curIndex := b.index
+	b.rawAdd(data, len(data), curIndex)
+	b.index++
+
+	return curIndex
+}
+
+func (b *Buffer) AddWithSpace(data []rune, space int) int {
+	if space < 0 {
+		panic("space must be >= 0")
+	}
+
+	b.mtx.Lock()
+	defer b.mtx.Unlock()
+
+	curIndex := b.index
+	b.rawAdd(data, space, curIndex)
+	b.index++
+
+	return curIndex
+}
+
+func (b *Buffer) Append(id int, data ...rune) {
+	b.mtx.Lock()
+	defer b.mtx.Unlock()
+
+	item := b.items[id]
+	if item.space >= len(data) {
+		written := b.rawWrite(data, id)
+		item.to += written
+		item.space -= written
+		b.items[id] = item
+		return
+	}
+
+	oldData := b.buf[item.from:item.to]
+	dataLen := len(oldData) + len(data)
+	from := len(b.buf)
+	b.rawAppend(oldData, 0)
+	b.rawAppend(data, dataLen)
+	item.from = from
+	item.to = from + dataLen
+	item.space = dataLen
+	b.items[id] = item
+}
+
+func (b *Buffer) Get(id int) []rune {
+	b.mtx.Lock()
+	defer b.mtx.Unlock()
+
+	item, ok := b.items[id]
+	if !ok {
+		return nil
+	}
+
+	return b.buf[item.from:item.to]
+}
+
+func (b *Buffer) rawAdd(data []rune, space int, id int) {
+	l := len(b.buf)
+	b.rawAppend(data, space)
+
+	b.items[id] = bufferItem{
+		from:  l,
+		to:    l + len(data),
+		space: space,
+	}
+}
+
+func (b *Buffer) rawAppend(data []rune, space int) {
+	b.buf = append(b.buf, data...)
+	for i := 0; i < space; i++ {
+		b.buf = append(b.buf, 0)
+	}
+}
+
+func (b *Buffer) rawWrite(data []rune, id int) int {
+	item := b.items[id]
+	idx := item.to
+	for i := idx; i < idx+len(data); i++ {
+		b.buf[i] = data[i-idx]
+	}
+
+	return len(data)
+}
+
+type bufferItem struct {
+	from  int
+	to    int
+	space int
+}
